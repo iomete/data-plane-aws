3d986a2013985cae5de2b5ccb789a6d061880aeb
vusal-iom <vusal@iomete.com>
2023-11-30 03:53:11 +0100

    Associate all route tables with s3 gateway

--- s3_vpc_endpoint.tf
+++ s3_vpc_endpoint.tf
@@ -1,13 +1,17 @@
+data "aws_route_tables" "all" {
+  vpc_id = module.vpc.vpc_id
+}
+
 resource "aws_vpc_endpoint" "this" {
   vpc_id            = module.vpc.vpc_id
   vpc_endpoint_type = "Gateway"
   service_name      = "com.amazonaws.${var.region}.s3"
-  route_table_ids   = [module.vpc.default_route_table_id]
+  route_table_ids   = data.aws_route_tables.all.ids
 
   tags = local.tags
 
   depends_on = [
-    module.eks
+    module.eks,
+    module.vpc
   ]
 }
-
